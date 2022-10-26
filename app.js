7812e44e20e0e54699a3b56d22d65a30fe8982a3
maxikaye <mr.max.kaye@gmail.com>
2022-10-26 18:27:24 -0400

    Add logic to display numeral buttons clicked

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Calculator by github.com/maxikaye
+// 2022 OCT
+const display = document.querySelector('.display');
+const numeralButtons = document.querySelectorAll('.numeral');
+let displayValue = 0;
+
 function add (a, b) {
     return a + b;
 }
@@ -37,3 +43,15 @@ function operate(operator, a, b) {
             return "Operator not recognized";
     }
 }
+
+function displayNumerals(key) {
+    console.log(key.innerHTML)
+    displayValue = key.innerHTML;
+    display.textContent = displayValue;
+}
+
+// numeral buttons event listeners
+numeralButtons.forEach( number => {
+    number.addEventListener('click', () => displayNumerals(number))
+});
+// function button event listeners
